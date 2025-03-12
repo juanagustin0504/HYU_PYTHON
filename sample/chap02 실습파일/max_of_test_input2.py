694df392ee4476be5b568aae2f6a1e0cca2c3b43
juanagustin0504 <juanagustin@naver.com>
2025-03-12 19:13:15 +0900

    [+] add 1&2 week document

---
+++ sample/chap02 실습파일/max_of_test_input2.py
@@ -0,0 +1,23 @@
+ # [Do it! 실습 2-3] 배열 원소의 최댓값을 구해서 출력하기(원솟값을 입력받음)
+
+from max import max_of
+
+print('배열의 최댓값을 구합니다.')
+print('주의: "End"를 입력하면 종료합니다.')
+
+number = 0
+x = []                  # 빈 리스트
+
+while True:
+    s = input(f'x[{number}]를 입력하세요.: ')
+    if s == 'End':
+        break
+    try:
+        x.append(int(s))    # 배열의 끝에 추가
+        number += 1
+    except ValueError:
+        print(f'"{s}"는 정수가 아닙니다. 다시입력해주세요.')
+        pass
+
+print(f'{number}개를 입력했습니다.')
+print(f'최댓값은 {max_of(x)}입니다.')
